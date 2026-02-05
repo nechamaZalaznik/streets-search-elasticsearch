720fa2c8a597196bce4308b75ffb638c5e9dac21
NechamaZalaznik <n627812@gmail.com>
2026-02-05 23:51:17 +0200

    Remove old files from streets-search-elasticsearch

--- streets-search-elasticsearch/src/types/index.ts
+++
@@ -1,7 +0,0 @@
-export type SearchMode = 'FREE' | 'EXACT' | 'PHRASE';
-
-export const SEARCH_OPTIONS = [
-  { id: 'FREE', label: 'חיפוש חופשי- רק לפי שם הרחוב', placeholder: 'הקלד שם רחוב לחיפוש...' },
-  { id: 'EXACT', label: 'חיפוש מדויק לפי כל שדה', placeholder: 'הקלד מילה לחיפוש מדויק...' },
-  { id: 'PHRASE', label: 'חיפוש ביטוי שלם- לפי כל שדה', placeholder: 'הקלד ביטוי מלא לחיפוש...' },
-] as const;
